c5f49d222ae31a309b38e17216fb10b447b01aa4
Tyler Dailey <tkdailey11@gmail.com>
2023-06-03 16:27:46 -0600

    CRA

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import About from "./About";
+ReactDOM.render(
+  <React.StrictMode>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App/>} />
+        <Route path="/about" element={<About/>}/>
+      </Routes>
+    </HashRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
